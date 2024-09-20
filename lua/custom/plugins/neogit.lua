d120fc68f2504690190ef12e43368aa3b0c9bed8
Jon Andre Briones <jonbrsoftware@gmail.com>
2024-09-20 14:48:54 -0700

    Configured NeoGit

---
+++ lua/custom/plugins/neogit.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+
+      -- Only one of these is needed.
+      'nvim-telescope/telescope.nvim', -- optional
+    },
+    config = true,
+    keys = {
+      { '<leader>gs', ':Neogit kind=split_below_all<CR>', desc = 'Open NeoGit' },
+    },
+  },
+}
