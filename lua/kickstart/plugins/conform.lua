d3d74351280c2e160e4a64f26b313cbc17ad6275
Jon Andre Briones <jonbrsoftware@gmail.com>
2024-09-22 00:02:24 -0700

    Added prettierd

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -33,6 +33,7 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        javascript = { 'prettierd', stop_after_first = true },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
