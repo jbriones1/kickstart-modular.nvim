e05c8c7e0b73205cd428c1eeec11b8089da264b5
Jon Andre Briones <jonbrsoftware@gmail.com>
2024-09-29 12:11:08 -0700

    Added LuaLine and configured status line

---
+++ lua/custom/plugins/lualine.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = function()
+      local filetype = {
+        'filetype',
+        icon_only = true,
+      }
+      local filename = {
+        'filename',
+        newfile_status = true,
+        path = 1,
+      }
+      return {
+        options = {
+          icons_enabled = vim.g.have_nerd_font,
+          theme = 'everforest',
+          ignore_focus = { 'neo-tree', 'NeogitStatus' },
+          section_separators = '',
+          component_separators = '',
+        },
+        sections = {
+          lualine_b = { 'branch' },
+          lualine_c = { filename },
+          lualine_x = { 'diff', 'diagnostics', filetype },
+        },
+      }
+    end,
+  },
+}
