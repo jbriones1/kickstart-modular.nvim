86637c508f4d9bb8b6207dee0aedde4fcdabff9b
Jon Andre Briones <jonbrsoftware@gmail.com>
2024-10-04 13:17:07 -0700

    Added ToggleTerm

---
+++ lua/custom/plugins/toggleterm.lua
@@ -0,0 +1,13 @@
+--  Toggles the terminal
+return {
+  {
+    {
+      'akinsho/toggleterm.nvim',
+      version = '*',
+      opts = {
+        open_mapping = '<C-\\>',
+        direction = 'float',
+      },
+    },
+  },
+}
