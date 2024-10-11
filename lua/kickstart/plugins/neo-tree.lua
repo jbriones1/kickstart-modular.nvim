c65d22e9903a2e6b671f38ba34460b053c9f1b0c
Jon Andre Briones <jonbrsoftware@gmail.com>
2024-10-11 15:38:28 -0700

    Added an Angular feature that can open a component and template in a split in a new tab

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -13,4 +13,36 @@ return {
   keys = {
     { '\\', ':Neotree action=focus source=filesystem position=right toggle=true reveal=true<CR>', desc = 'NeoTree reveal', silent = true },
   },
+  opts = {
+    filesystem = {
+      commands = {},
+      window = {
+        mappings = {
+          -- Angular mappings
+          -- Open the controller and template in a vertical split (controller | template)
+          ['<C-1>'] = function(state)
+            local node = state.tree:get_node()
+            if node == nil or node.type ~= 'file' then
+              return
+            end
+
+            local name = node.name
+            local path = state.tree:get_node().path
+            -- If it's a template, change path to the controller
+            if string.find(name, '.html$') ~= nil then
+              state.tree:get_node().path = string.gsub(path, '.html$', '.ts')
+            -- If it's not a controller, then just return
+            elseif string.find(name, '.ts$') == nil then
+              return
+            end
+
+            local cmds = require 'neo-tree.sources.filesystem.commands'
+            cmds.open_tabnew(state)
+            state.tree:get_node().path = string.gsub(path, '.ts$', '.html')
+            cmds.open_vsplit(state)
+          end,
+        },
+      },
+    },
+  },
 }
