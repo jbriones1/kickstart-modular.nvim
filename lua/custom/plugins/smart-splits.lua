8a28d529189834e619cb51389ebed387811678b1
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-02-13 17:44:05 -0800

    Added smart splits

---
+++ lua/custom/plugins/smart-splits.lua
@@ -0,0 +1,17 @@
+return {
+  'mrjones2014/smart-splits.nvim',
+  lazy = false,
+  config = function()
+    vim.keymap.set('n', '<A-r>', require('smart-splits').start_resize_mode, { desc = 'Move focus down' })
+    -- Move between splits
+    vim.keymap.set('n', '<C-h>', require('smart-splits').move_cursor_left, { desc = 'Move focus to the left' })
+    vim.keymap.set('n', '<C-l>', require('smart-splits').move_cursor_right, { desc = 'Move focus to the right' })
+    vim.keymap.set('n', '<C-j>', require('smart-splits').move_cursor_down, { desc = 'Move focus up' })
+    vim.keymap.set('n', '<C-k>', require('smart-splits').move_cursor_up, { desc = 'Move focus down' })
+    -- Swapping buffers between windows
+    vim.keymap.set('n', '<leader>h', require('smart-splits').swap_buf_left, { desc = 'Swap with left panel' })
+    vim.keymap.set('n', '<leader>j', require('smart-splits').swap_buf_down, { desc = 'Swap with left panel' })
+    vim.keymap.set('n', '<leader>k', require('smart-splits').swap_buf_up, { desc = 'Swap with left panel' })
+    vim.keymap.set('n', '<leader>l', require('smart-splits').swap_buf_right, { desc = 'Swap with left panel' })
+  end,
+}
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -32,11 +32,11 @@ vim.keymap.set('i', '<C-BS>', '<C-w>')
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
+-- vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+-- vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+-- vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+-- vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+--
 -- Keybinds for tab control
 vim.keymap.set({ 'n', 'v' }, '<S-t>', ':tabp<CR>', { desc = 'Go to previous tab', silent = true })
 vim.keymap.set({ 'n', 'v' }, '<S-Tab>', ':tabn<CR>', { desc = 'Go to next tab', silent = true })
