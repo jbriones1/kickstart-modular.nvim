0e1b8fde9f6f1f1cdfbfd11aa82c66d172e2c2be
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-05-15 17:53:36 -0700

    Added telescope hotkey to open the file in a split using C-x

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -85,6 +85,7 @@ return {
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("" for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<C-x>', builtin.select_horizontal, { desc = 'Open in horizontal split' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
