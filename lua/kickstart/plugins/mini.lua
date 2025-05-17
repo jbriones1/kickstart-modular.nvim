8451ac7bad579f5a19e7d60ddb4e9c76e35e7aed
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-05-17 11:03:35 -0700

    Tried updating mini.surround to include angular custom HTML tags

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -15,7 +15,22 @@ return {
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      require('mini.surround').setup {
+        opts = {
+          custom_surroundings = {
+            t = {
+              input = { '<([%p%w-]-)%f[^<%p%w-][^<>]->.-</%1>', '^<.->().*()</[^/]->$' },
+              output = function()
+                local tag_name = MiniSurround.user_input 'Tag name'
+                if tag_name == nil then
+                  return nil
+                end
+                return { left = tag_name, right = tag_name }
+              end,
+            },
+          },
+        },
+      }
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
