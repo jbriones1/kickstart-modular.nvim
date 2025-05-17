414c82d65810a5f7b87e7aa98d80c2c2958828a6
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-05-17 13:09:52 -0700

    Disabled smart splits buffer swapping.

--- lua/custom/plugins/smart-splits.lua
+++ lua/custom/plugins/smart-splits.lua
@@ -8,9 +8,9 @@ return {
     vim.keymap.set('n', '<C-j>', require('smart-splits').move_cursor_down, { desc = 'Move focus up' })
     vim.keymap.set('n', '<C-k>', require('smart-splits').move_cursor_up, { desc = 'Move focus down' })
     -- Swapping buffers between windows
-    vim.keymap.set('n', '<leader>h', require('smart-splits').swap_buf_left, { desc = 'Swap with left panel' })
-    vim.keymap.set('n', '<leader>j', require('smart-splits').swap_buf_down, { desc = 'Swap with below panel' })
-    vim.keymap.set('n', '<leader>k', require('smart-splits').swap_buf_up, { desc = 'Swap with above panel' })
-    vim.keymap.set('n', '<leader>l', require('smart-splits').swap_buf_right, { desc = 'Swap with right panel' })
+    -- vim.keymap.set('n', '<leader>h', require('smart-splits').swap_buf_left, { desc = 'Swap with left panel' })
+    -- vim.keymap.set('n', '<leader>j', require('smart-splits').swap_buf_down, { desc = 'Swap with below panel' })
+    -- vim.keymap.set('n', '<leader>k', require('smart-splits').swap_buf_up, { desc = 'Swap with above panel' })
+    -- vim.keymap.set('n', '<leader>l', require('smart-splits').swap_buf_right, { desc = 'Swap with right panel' })
   end,
 }
