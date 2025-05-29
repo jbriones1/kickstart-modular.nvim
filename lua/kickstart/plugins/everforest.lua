0b2007944d2d5c9388e5045c04aa81863c645537
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-05-29 10:48:22 -0700

    Changed which everforest I'm using

--- lua/kickstart/plugins/everforest.lua
+++ lua/kickstart/plugins/everforest.lua
@@ -1,13 +1,31 @@
 return {
+  -- Vim everforest
+  -- {
+  --   'sainnhe/everforest',
+  --   priority = 1000,
+  --   init = function()
+  --     vim.g.everforest_background = 'hard'
+  --     vim.g.everforest_better_performance = 1
+  --     vim.o.background = 'dark'
+  --     vim.cmd.colorscheme 'everforest'
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
   {
-    'sainnhe/everforest',
-    priority = 1000,
-    init = function()
-      vim.g.everforest_background = 'hard'
-      vim.g.everforest_better_performance = 1
-      vim.o.background = 'dark'
-      vim.cmd.colorscheme 'everforest'
-      vim.cmd.hi 'Comment gui=none'
+    'neanias/everforest-nvim',
+    version = false,
+    lazy = false,
+    priority = 1000, -- make sure to load this before all the other start plugins
+    -- Optional; default configuration will be used if setup isn't called.
+    config = function()
+      require('everforest').setup {
+        -- Your config here
+        background = 'hard',
+        disable_italic_comments = true,
+        diagnostic_line_highlight = true,
+        diagnostic_virtual_text = 'grey',
+      }
+      require('everforest').load()
     end,
   },
 }
