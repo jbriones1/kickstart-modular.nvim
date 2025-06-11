67d1a3784858bef983fdb7887943c56abb67478a
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-06-11 10:17:15 -0700

    feat: Swapped direction of tab shifting
    * T goes forward, <S-t> goes backwards

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -44,8 +44,8 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
 -- Keybinds for tab control
-vim.keymap.set({ 'n', 'v' }, '<S-t>', ':tabp<CR>', { desc = 'Go to previous tab', silent = true })
-vim.keymap.set({ 'n', 'v' }, '<S-Tab>', ':tabn<CR>', { desc = 'Go to next tab', silent = true })
+vim.keymap.set({ 'n', 'v' }, '<S-Tab>', ':tabp<CR>', { desc = 'Go to previous tab', silent = true })
+vim.keymap.set({ 'n', 'v' }, '<S-t>', ':tabn<CR>', { desc = 'Go to next tab', silent = true })
 vim.keymap.set({ 'n', 'v' }, '<C-t>w', ':tabclose<CR>', { desc = 'Close current tab', silent = true })
 vim.keymap.set({ 'n', 'v' }, '<C-t>t', ':tabnew<CR>', { desc = 'Open a new tab', silent = true })
 
