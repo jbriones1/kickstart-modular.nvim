4cbadcbb99fd187ff57cdd6a49fc90a2db59517f
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-06-25 13:48:29 -0700

    feat: Changed the formatter to use the LSP for C++

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -53,7 +53,7 @@ return {
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = { c = true }
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
         else
@@ -72,7 +72,7 @@ return {
         htmlangular = { 'prettierd' },
         json = { 'prettierd' },
         python = { 'isort', 'black' },
-        cpp = { 'clang-format' },
+        -- cpp = { 'clang-format' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
