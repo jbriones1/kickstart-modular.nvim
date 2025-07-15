9b76a8ef72a525777cf8ab1e3de108fa0a2b7b1a
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-07-15 11:34:56 -0700

    feat: Re-enabled Flash search when changing

--- lua/custom/plugins/flash.lua
+++ lua/custom/plugins/flash.lua
@@ -33,7 +33,7 @@ return {
     { "<leader>m", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     { "<leader>,", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     { "<C-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     -- Disables T to preserve tab shifting
   },
