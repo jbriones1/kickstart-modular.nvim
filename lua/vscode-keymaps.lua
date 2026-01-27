d7bb7f5fd2915cb758162f9d90b1daaf473a0bc2
Jon Andre Briones <jonbrsoftware@gmail.com>
2026-01-26 17:33:32 -0800

    feat(vscode): add keymap to view source control

--- lua/vscode-keymaps.lua
+++ lua/vscode-keymaps.lua
@@ -4,3 +4,6 @@ vim.keymap.set('n', '<leader>sf', '<cmd>lua require("vscode").action("workbench.
 -- Code Tools
 vim.keymap.set('n', '<leader>ca', '<cmd>lua require("vscode").action("editor.action.quickFix")<CR>', { desc = '[C]ode [A]ctions' })
 vim.keymap.set('n', '<leader>f', '<cmd>lua require("vscode").action("editor.action.formatDocument")<CR>', { desc = '[F]ormat Code' })
+
+-- Source Control
+vim.keymap.set('n', '<leader>gs', '<cmd>lua require("vscode").action("workbench.view.scm")<CR>', { desc = '[G]it [S]tatus' })
