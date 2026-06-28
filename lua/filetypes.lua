c22023ab0cd10df98330ca47e2f1eae21aba2801
Jon Andre Briones <jonbrsoftware@gmail.com>
2026-06-28 01:44:18 -0700

    feat(mdx): add tree-sitter highlighting

--- lua/filetypes.lua
+++ lua/filetypes.lua
@@ -1,5 +1,10 @@
 vim.filetype.add {
   pattern = {
     ['.*%.component%.html'] = 'htmlangular',
+    ['.*%.screen%.html'] = 'htmlangular',
+    ['.*%.widget%.html'] = 'htmlangular',
+  },
+  extension = {
+    mdx = 'markdown',
   },
 }
