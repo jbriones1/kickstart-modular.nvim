e64d983121f5be21f85d52966a3df0050b68b5b7
Jon Andre Briones <jonbrsoftware@gmail.com>
2026-08-01 17:44:48 -0700

    fix: remove unused diagnostic comment

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -136,47 +136,6 @@ return {
           end
         end,
       })
-      -- <<<<<<< HEAD
-      --       -- Diagnostic Config
-      --       -- See :help vim.diagnostic.Opts
-      --       vim.diagnostic.config {
-      --         severity_sort = true,
-      --         float = { border = 'rounded', source = 'if_many' },
-      --         underline = { severity = vim.diagnostic.severity.ERROR },
-      --         signs = vim.g.have_nerd_font and {
-      --           text = {
-      --             [vim.diagnostic.severity.ERROR] = '󰅚 ',
-      --             [vim.diagnostic.severity.WARN] = '󰀪 ',
-      --             [vim.diagnostic.severity.INFO] = '󰋽 ',
-      --             [vim.diagnostic.severity.HINT] = '󰌶 ',
-      --           },
-      --         } or {},
-      --         virtual_text = {
-      --           source = 'if_many',
-      --           spacing = 2,
-      --           format = function(diagnostic)
-      --             local diagnostic_message = {
-      --               [vim.diagnostic.severity.ERROR] = diagnostic.message,
-      --               [vim.diagnostic.severity.WARN] = diagnostic.message,
-      --               [vim.diagnostic.severity.INFO] = diagnostic.message,
-      --               [vim.diagnostic.severity.HINT] = diagnostic.message,
-      --             }
-      --             return diagnostic_message[diagnostic.severity]
-      --           end,
-      --         },
-      --       }
-
-      --       -- Change diagnostic symbols in the sign column (gutter)
-      --       --if vim.g.have_nerd_font then
-      --       --  local signs = { ERROR = '', WARN = '', INFO = '', HINT = '' }
-      --       --local diagnostic_signs = {}
-      --       --for type, icon in pairs(signs) do
-      --       --  diagnostic_signs[vim.diagnostic.severity[type]] = icon
-      --       --end
-      --       --vim.diagnostic.config { signs = { text = diagnostic_signs } }
-      --       --end
-      -- =======
-      -- >>>>>>> origin/master
 
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
